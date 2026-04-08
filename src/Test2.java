6e7b723b085285dee6cfd3b5f0718125d73e04fb
Camper02 <egor_shumanovich@mail.ru>
2026-04-08 16:02:38 +0200

    второй тестовый класс для понимания коммитов

--- src/Test2.java
+++ src/Test2.java
@@ -2,6 +2,7 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("new hello");
         System.out.println("new hello2");
+        System.out.println("new hello3");
     }
 
 }
